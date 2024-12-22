e35652d11a4ce8b6172496f126155152d9328dcb
KasineTBou <robertfomin55@gmail.com>
2024-12-22 22:10:36 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+<script>
+function openModal(imageSrc) {
+    const modal = document.getElementById('modal');
+     const modalImage = document.getElementById('modal-image');
+    modalImage.src = imageSrc;
+    modal.style.display = 'block';
+}
 
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+</script>
